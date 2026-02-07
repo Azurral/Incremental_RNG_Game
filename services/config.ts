0c5248927b856531a7c35c6c7d385f35cb6284a8
rosalejos, sean earl a. <seanrosalejosismyemail@gmail.com>
2026-02-07 16:06:39 +0800

    Fix firebase imports and storageBucket in services/config.ts

--- services/config.ts
+++ services/config.ts
@@ -1,11 +1,12 @@
-import {initializeApp} from "firebase/app";
-import {}
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCPhURWgl4SpQBGosU1foFzzF7wRFOnGGI",
   authDomain: "incrementalrnggame-50554.firebaseapp.com",
   projectId: "incrementalrnggame-50554",
-  storageBucket: "incrementalrnggame-50554.firebasestorage.app",
+  storageBucket: "incrementalrnggame-50554.appspot.com",
   messagingSenderId: "949571648099",
   appId: "1:949571648099:web:1bed76509faa967755d656",
   measurementId: "G-LNCLNPRSPY"
